9d10e865602378efb4cb00c8dd2ca03492d50124
rednafi <redowan.nafi@gmail.com>
2019-06-05 14:40:26 +0600

    overhauling the python script

--- create.py
+++ create.py
@@ -1,20 +1,40 @@
 import sys
 import os
+from subprocess import call
 from github import Github
+import subprocess
 
-path = "./"
+# extracting project path, github username and password from bashrc
+
 username = os.environ["GITHUB_USERNAME"]
-password = os.environ["GITHUB_PASSWORD"]  
+password = os.environ["GITHUB_PASSWORD"]
+path = os.environ["PROJECT_PATH"]
 
+# repository name
+repo_name = str(sys.argv[1])
 
-def create():
-    folder_name = str(sys.argv[1])
-    os.makedirs(path + folder_name))
+# creation of local repository
+if not os.path.exists(path + repo_name):
+    os.mkdir(path + repo_name)
     user = Github(username, password).get_user()
-    repo = user.create_repo(sys.argv[1])
-    print("Succesfully created repository {}".format(sys.argv[1]))
+    repo = user.create_repo(repo_name)
 
+    cmd = """
+        cd {0}{1}
+        git init
+        git remote add origin git@github.com:{2}/{3}.git
+        touch README.md
+        git add .
+        git commit -m "Initial commit"
+        git push -u origin master
+        code.
+        """.format(
+        path, repo_name, username, repo_name
+    )
+    subprocess.check_output(cmd, shell=False)
 
-if __name__ == "__main__":
-    create()
-print(username, password)
+    print("succesfully created repository {}".format(repo_name))
+
+else:
+    print("local repository already exists")
+
