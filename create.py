4ce475e258a0cb2430f55fe350fe82c137aa77c7
rednafi <redowan.nafi@gmail.com>
2019-06-05 15:32:28 +0600

    create.py completed

--- create.py
+++ create.py
@@ -1,40 +1,52 @@
 import sys
 import os
-from subprocess import call
 from github import Github
 import subprocess
 
 # extracting project path, github username and password from bashrc
+try:
+    username = os.environ["GITHUB_USERNAME"]
+    password = os.environ["GITHUB_PASSWORD"]
+    path = os.environ["PROJECT_PATH"]
 
-username = os.environ["GITHUB_USERNAME"]
-password = os.environ["GITHUB_PASSWORD"]
-path = os.environ["PROJECT_PATH"]
+except:
+    print("bashrc variables are not found")
 
 # repository name
 repo_name = str(sys.argv[1])
+full_path = path + repo_name
 
 # creation of local repository
-if not os.path.exists(path + repo_name):
-    os.mkdir(path + repo_name)
-    user = Github(username, password).get_user()
-    repo = user.create_repo(repo_name)
 
-    cmd = """
-        cd {0}{1}
-        git init
-        git remote add origin git@github.com:{2}/{3}.git
-        touch README.md
-        git add .
-        git commit -m "Initial commit"
-        git push -u origin master
-        code.
-        """.format(
-        path, repo_name, username, repo_name
-    )
-    subprocess.check_output(cmd, shell=False)
+if os.path.exists(full_path):
+    print("local repository '{}' exists".format(repo_name))
 
-    print("succesfully created repository {}".format(repo_name))
+elif not os.path.exists(full_path):
+    os.mkdir(full_path)
+    try:
+        user = Github(username, password).get_user()
+        repo = user.create_repo(repo_name)
+        cmd = """
+            cd {0}
+            git init
+            git remote add origin git@github.com:{1}/{2}.git
+            touch README.md
+            git add .
+            git commit -m "Initial commit"
+            git push -u origin master
+            code {0}
+            """.format(
+            full_path, username, repo_name
+        )
+        subprocess.check_output(cmd, shell=True)
+
+        print("local and remote repository successfully created")
+
+    except:
+        print("remote repository '{}' already exists".format(repo_name))
 
 else:
-    print("local repository already exists")
+    pass 
+
+
 
