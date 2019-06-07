1c135d1f55655c072af6a4bcd93d4530540ba548
rednafi <redowan.nafi@gmail.com>
2019-06-07 16:15:13 +0600

    version 0.3

--- src/create.py
+++ src/create.py
@@ -30,30 +30,32 @@ def protomate():
         github_username = str(input("github_username :"))
         github_password = str(getpass.getpass(prompt="github_password: "))
 
-        try:
-            user = Github(github_username, github_password).get_user()
-            
-            try:
-                repo = user.create_repo(repo_name)
-                cmd = """
-                    cd {0}
-                    git init
-                    git remote add origin git@github.com:{1}/{2}.git
-                    touch README.md
-                    git add .
-                    git commit -m "Initial commit"
-                    git push -u origin master
-                    code {0}
-                    """.format(
-                    local_path, github_username, repo_name
-                )
-                subprocess.check_output(cmd, shell=True)
-                print("local and remote repository successfully created")
+        user = Github(github_username, github_password).get_user()
 
-            except:
-                print("remote repository '{}' already exists".format(repo_name))
-                os.rmdir(local_path)
-                
+        try:
+            if user.login:
+                print("login successful")
+                try:
+                    repo = user.create_repo(repo_name)
+                    cmd = """
+                        cd {0}
+                        git init
+                        git remote add origin git@github.com:{1}/{2}.git
+                        touch README.md
+                        git add .
+                        git commit -m "Initial commit"
+                        git push -u origin master
+                        code {0}
+                        """.format(
+                        local_path, github_username, repo_name
+                    )
+                    subprocess.check_output(cmd, shell=True)
+                    print("local and remote repository successfully created")
+
+                except:
+                    print("remote repository '{}' already exists".format(repo_name))
+                    os.rmdir(local_path)
+
         except:
             print("incorrect username or password")
             os.rmdir(local_path)
@@ -61,3 +63,6 @@ def protomate():
 
 if __name__ == "__main__":
     protomate()
+
+
+
