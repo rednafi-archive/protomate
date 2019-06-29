992b5e10ba06d319d47322db44aaffc002d9d2fb
rednafi <redowan.nafi@gmail.com>
2019-06-29 19:44:37 +0600

    made pip installable v0.2.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = "https://github.com/rednafi/protomate"
 EMAIL = "redowan.nafi@gmail.com"
 AUTHOR = "Redowan"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
