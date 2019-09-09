1ec1de0ca2b5f479b8c97d41b21374fa170f2ac4
rednafi <redowan.nafi@gmail.com>
2019-09-09 10:33:19 +0600

    released v0.5.1

---
+++ protomate/cli_prompts.py
@@ -0,0 +1,93 @@
+from pyfiglet import figlet_format
+import questionary
+from prompt_toolkit.styles import Style
+from termcolor import cprint
+import colorama
+import sys
+from protomate.repo_auths import is_pass_saved, save_pass, retrieve_pass
+
+colorama.init(strip=not sys.stdout.isatty())
+
+
+def draw_ascii_banner():
+    """
+    Draw Protomate banner !!!
+    """
+    text = "Protomate"
+    ascii_banner = figlet_format(text, font="standard")
+    cprint(ascii_banner, "cyan", attrs=["bold"])
+
+
+def cli():
+    """CLI function that shows a list of questions regarding
+    github credentials and other functionalities.
+
+    Returns
+    -------
+    github_username : str
+        Github user name where the repository will be created.
+    github_password : str
+        Github password.
+    repo_name : str
+        Desired repository name.
+    repo_type : str
+        Whether the repository will be public or private.
+    gitignore : str
+        Question regarding whether to add a gitignore file or not.
+        If yes then asks for the name of the gitignore language.
+
+    """
+
+    style = Style(
+        [
+            ("qmark", "fg:#e91e63 bold"),
+            ("answer", "fg:#fac731"),
+            ("instruction", "fg:#f06292"),
+            ("separator", "fg:#cc5454"),
+            ("selected", "fg:#7fc97f"),
+            ("pointer", "fg:#fdc086"),
+            ("question", "fg:#d3d7cF"),
+        ]
+    )
+
+    github_username = questionary.text("GitHub Username:", style=style).ask()
+
+    PASS_SAVED = is_pass_saved(github_username)
+    if not PASS_SAVED:
+        github_password = (
+            questionary.password("GitHub Password:", style=style)
+            .skip_if(PASS_SAVED, default=False)
+            .ask()
+        )
+    else:
+        github_password = retrieve_pass(github_username)
+
+    password_save = (
+        questionary.select(
+            "Do you want to save your password?", choices=["Yes", "No"], style=style
+        )
+        .skip_if(PASS_SAVED, default=False)
+        .ask()
+    )
+
+    repo_name = questionary.text("Repository Name:", style=style).ask()
+    repo_type = questionary.select(
+        "Repository Type:", choices=["Public", "Private"], style=style
+    ).ask()
+
+    gitignore = questionary.text(
+        """Gitignore(Optional):
+            Please enter the desired language name to create
+            .gitignore file, press enter if you don't want to:
+            """,
+        style=style,
+    ).ask()
+
+    return (
+        github_username,
+        github_password,
+        password_save,
+        repo_name,
+        repo_type,
+        gitignore,
+    )
